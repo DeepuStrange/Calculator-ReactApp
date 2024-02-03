f4cdf1dec6258794ddde3a84dc19c0255cdbaf98
pradeep <pradeep9100234360@gmail.com>
2024-02-03 16:58:47 +0530

    changed Css

--- src/App.js
+++ src/App.js
@@ -28,58 +28,44 @@ function App(){
     <div style={{
       display:"grid",
       justifyContent:"center",
+      marginTop:"30px"
 }}>
+  <h1 style={{textAlign:"center"}}>Calculator</h1>
       <div style={{
         display:"grid",
         rowGap:20,
         justifyItems:"center",
+        border:"1px solid grey",
+        padding:"20px",
+        borderRadius:"10px"
       }}>
         <input id="text" style={{
           width:"100%",
           height:"50px",
-          textAlign:"right"
+          textAlign:"right",
+          filter:"drop-shadow(3px 3px 1px blue)",
         }} type="text" disabled></input>
-        <div style={{
-          display:"grid",
-          rowGap:20,
-          backgroundColor:"slategray",
-          padding:10
-        }}>
-        <div style={{
-          display:"grid",
-          gridTemplateColumns:"auto auto 35px 33px",
-          gap:35,
-          alignItems:"center",
-}}>
-          <button onClick={clearResult} className="btn1">AC</button>
-          <button onClick={backSpace} className="btn1">Del</button>
-          <button onClick={()=>{captureValue("%")}} className="btn1">%</button>
-          <button onClick={()=>{captureValue("+")}} className="btn4">+</button>
-        </div>
-        <div className="Adjust">
-        <button onClick={()=>{captureValue("1")}} className="btn1">1</button>
-        <button onClick={()=>{captureValue("2")}} className="btn1">2</button>
-        <button onClick={()=>{captureValue("3")}} className="btn1">3</button>
-        <button onClick={()=>{captureValue("-")}} className="btn4">-</button>
-        </div>
-        <div className="Adjust">
-          <button onClick={()=>{captureValue("4")}} className="btn1">4</button>
-          <button onClick={()=>{captureValue("5")}} className="btn1">5</button>
-          <button onClick={()=>{captureValue("6")}} className="btn1">6</button>
-          <button onClick={()=>{captureValue("*")}} className="btn4">*</button>
-        </div>
-        <div className="Adjust">
-          <button onClick={()=>{captureValue("7")}} className="btn1">7</button>
-          <button onClick={()=>{captureValue("8")}} className="btn1">8</button>
-          <button onClick={()=>{captureValue("9")}} className="btn1">9</button>
-          <button onClick={()=>{captureValue("/")}} className="btn4">/</button>
-        </div>
-        <div className="Adjust">
-          <button onClick={()=>{captureValue("00")}} className="btn1">00</button>
-          <button onClick={()=>{captureValue("0")}} className="btn1">0</button>
-          <button onClick={()=>{captureValue(".")}} className="btn1">.</button>
-          <button onClick={result} className="btn4">=</button>
-        </div>
+        <div className="btns">
+          <button onClick={clearResult} >AC</button>
+          <button onClick={backSpace} >Del</button>
+          <button onClick={()=>{captureValue("%")}} >%</button>
+          <button onClick={()=>{captureValue("+")}} >+</button>
+          <button onClick={()=>{captureValue("1")}} >1</button>
+          <button onClick={()=>{captureValue("2")}} >2</button>
+          <button onClick={()=>{captureValue("3")}} >3</button>
+          <button onClick={()=>{captureValue("-")}} >-</button>
+          <button onClick={()=>{captureValue("4")}} >4</button>
+          <button onClick={()=>{captureValue("5")}} >5</button>
+          <button onClick={()=>{captureValue("6")}} >6</button>
+          <button onClick={()=>{captureValue("*")}} >*</button>
+          <button onClick={()=>{captureValue("7")}} >7</button>
+          <button onClick={()=>{captureValue("8")}} >8</button>
+          <button onClick={()=>{captureValue("9")}} >9</button>
+          <button onClick={()=>{captureValue("/")}} >/</button>
+          <button onClick={()=>{captureValue("00")}} >00</button>
+          <button onClick={()=>{captureValue("0")}} >0</button>
+          <button onClick={()=>{captureValue(".")}} >.</button>
+          <button onClick={result}>=</button>
         </div>
       </div>
     </div>
